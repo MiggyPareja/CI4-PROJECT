bca61fe2790f9afa61d02d6fdfeb87307b893d88
Your Name <you@example.com>
2023-05-06 08:05:37 +0800

    added jumbotron to welcome page

--- app/Views/welcome_Page.php
+++ app/Views/welcome_Page.php
@@ -10,17 +10,15 @@
 </head>
 <body>
     
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-6">
-                <a href="<?=base_url()?>register" class="btn btn-primary btn-lg btn-block">Register</a>
-            </div>
-            <div class="col-md-6">
-                <a href="<?=base_url()?>login" class="btn btn-secondary btn-lg btn-block">Login</a>
-            </div>
+    <div class="jumbotron jumbotron-fluid bg-light">
+        <div class="container">
+            <h1 class="display-4">Product Handler</h1>
+            <p class="lead">Product Handler is a reliable and efficient platform that enables businesses of all sizes to manage their products with ease. Our user-friendly interface allows you to effortlessly organize, categorize, and track your inventory in real-time, ensuring that you always have accurate and up-to-date information at your fingertips.</p>
+            <a href="<?=base_url()?>register" class="btn btn-primary btn-lg">Register</a>
+            <a href="<?=base_url()?>login" class="btn btn-secondary btn-lg">Login</a>
         </div>
     </div>
-    
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
